5b4d5cd274ef6801d320e70b44091a6fefc1cd9f
gyualord <gyualord@gmail.com>
2021-05-07 14:26:33 +0900

    2021.05.07 시험 3

---
+++ Java Class/Workspace/testJava_1/src/testJava_1/Portfolio_8.java
@@ -0,0 +1,16 @@
+package testJava_1;
+
+public class Portfolio_8 {
+	public static void main(String[] args) {
+		int max = 0;
+		int[i] array = Math.random(i = 0; i <=100; i++);
+		
+		for (int i = 0; i < array.lenth; i++) {
+			if(max < array[i]) {
+				max = array[i]
+			}
+		}
+		System.out.println("max : " + max);
+		
+	}
+}
