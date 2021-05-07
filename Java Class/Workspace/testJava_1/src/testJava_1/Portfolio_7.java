915fb5d57fc93fc76f61cec00fffc71d62717fd1
gyualord <gyualord@gmail.com>
2021-05-07 15:08:11 +0900

    2021.05.07 시험 마지막

--- Java Class/Workspace/testJava_1/src/testJava_1/Portfolio_7.java
+++ Java Class/Workspace/testJava_1/src/testJava_1/Portfolio_7.java
@@ -10,5 +10,13 @@ public class Portfolio_7 {
 				}
 			}
 		}
+		for(int i = 4; i >= 1; i++) {
+			for(int j = 1; j <= i; j++) {
+				System.out.printf("*");
+				if(j == i) {
+					System.out.println();
+				}
+			}
+		}
 	}
 }
